8d89bb49c4e2ad84cad28f9cd4a10901a1f685b3
Beatriz Llamas <72502042+beallasai@users.noreply.github.com>
2022-07-27 19:43:26 +0200

    Improved code structure

--- dummyBot.py
+++ dummyBot.py
@@ -1,24 +1,34 @@
+from sc2 import maps  
 from sc2.bot_ai import BotAI  
-from sc2.data import Difficulty, Race 
+from sc2.data import Difficulty, Race, Result
 from sc2.main import run_game  
 from sc2.player import Bot, Computer  
-from sc2 import maps  
 from sc2.ids.unit_typeid import UnitTypeId
+
+import time
 
 class dummyBot(BotAI):
 
+    
     async def on_step(self,iteration):
-        await self.distribute_workers()             #distribución de obreros para recolectar
 
-        command_center = self.townhalls.random      #entre los centros de mando que tengamos
+        await self.distribute_workers()         #distribución de obreros para recolectar
 
-        if command_center.is_idle and self.can_afford(UnitTypeId.SCV):      #se crean obreros siempre que se pueda
-            command_center.train(UnitTypeId.SCV)
+        await self.train_workers()              #creación de obreros
 
-        #mandar a un scout al empezar
-        if iteration==0:
+        await self.initial_scout(iteration)     #scout inicial
+
+
+    async def train_workers(self):
+        for command_center in self.structures(UnitTypeId.COMMANDCENTER).ready:
+            if command_center.is_idle and self.can_afford(UnitTypeId.SCV):      
+                command_center.train(UnitTypeId.SCV)
+
+    async def initial_scout(self, iteration):
+        if iteration==0:                            
             scout = self.workers[0]
             scout.attack(self.enemy_start_locations[0])
+
 
 
 run_game( 
