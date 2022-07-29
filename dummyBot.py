ed7998d97ab9d0890e3f0ae0a807d4a80a71b471
Beatriz Llamas <72502042+beallasai@users.noreply.github.com>
2022-07-29 13:04:14 +0200

    Add some metrics

--- dummyBot.py
+++ dummyBot.py
@@ -9,8 +9,23 @@ import time
 
 class dummyBot(BotAI):
 
+    def on_end(self, game_result):
+        print('- - - - - - - - - - - - - - - - - - - - - - -')
+        print('- - - - - - - - - GAME STATS - - - - - - - - -')
+        print(game_result, ' |  Total time:', self.time)
+        print('- - - - - - - - - - - - - - - - - - - - - - -')
+        print('- - - - - - - - - OWN STATS - - - - - - - - -')
+        print('Minerals:',self.minerals, ' |  Gas:',self.vespene, ' |  Supply:',self.supply_used,'/',self.supply_cap)
+        print('Units:',self.units.amount, ' |  Structures:',self.structures.amount)
+        print('- - - - - - - - - - - - - - - - - - - - - - -')
+        print('- - - - - - - - - ENEMY STATS - - - - - - - - -')
+        print('Units:',self.enemy_units.amount, 'Structures:',self.enemy_structures.amount)
+
     
     async def on_step(self,iteration):
+
+        print('iteration:',iteration)
+
 
         await self.distribute_workers()         #distribución de obreros para recolectar
 
@@ -21,11 +36,12 @@ class dummyBot(BotAI):
 
     async def train_workers(self):
         for command_center in self.structures(UnitTypeId.COMMANDCENTER).ready:
-            if command_center.is_idle and self.can_afford(UnitTypeId.SCV):      
+            if command_center.is_idle and self.can_afford(UnitTypeId.SCV):      #se crean obreros siempre que se pueda
                 command_center.train(UnitTypeId.SCV)
 
+    
     async def initial_scout(self, iteration):
-        if iteration==0:                            
+        if iteration==0:                            #mandar a un scout al empezar
             scout = self.workers[0]
             scout.attack(self.enemy_start_locations[0])
 
